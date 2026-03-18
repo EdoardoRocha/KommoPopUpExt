2f70c790d6e748d23e04cb3815b7aa4dd36c5768
EdoardoRocha <pazedoardo@gmail.com>
2026-03-18 00:09:49 -0300

    feat: drag drop and minimize popup

--- content.js
+++ content.js
@@ -1,33 +1,64 @@
 // Criar o container do pop-up
-const popup = document.createElement('div');
-const popupclosed = document.createElement("div");
-popupclosed.id = 'meu-sistema-popup-fechado'
-popupclosed.style.display = 'none';
-popup.id = 'meu-sistema-popup';
+const popup = document.createElement("div");
+popup.id = "meu-sistema-popup";
+popup.style.position = "fixed"; // Melhor usar fixed para popups
+popup.style.zIndex = "9999";
 popup.innerHTML = `
-  <div class="header">
+  <div class="header" id="popup-header" style="cursor: move;">
     <span>ChatZon</span>
     <button id="close-popup">X</button>
   </div>
-  <iframe src="https://chat-zon.vercel.app" frameborder="0"></iframe>
+  <iframe src="https://chat-zon.vercel.app" id="view-app-chatzon" frameborder="0"></iframe>
 `;
-popupclosed.innerHTML = `
-  <div class="header">
-    <span>ChatZon</span>
-    <button id="open-popup">-</button>
-  </div>
-`
 
 document.body.appendChild(popup);
-document.body.appendChild(popupclosed);
 
+const app = document.getElementById("view-app-chatzon");
+const popupBox = document.getElementById("meu-sistema-popup");
+const header = document.getElementById("popup-header");
+const btnPopUp = document.getElementById("close-popup")
 
-document.getElementById('close-popup').onclick = () => {
-  popup.style.display = 'none';
-  popupclosed.style.display = 'block';
+btnPopUp.onclick = () => {
+  if (app.style.display !== "none") {
+    btnPopUp.innerHTML = "-";
+    app.style.display = "none";
+    popupBox.style.height = "auto";
+  } else {
+    btnPopUp.innerHTML = "X";
+    app.style.display = "block";
+    popupBox.style.height = "540px";
+  }
 };
-  
-document.getElementById('close-popup').onclick = () => {
-  popup.style.display = 'none';
-  popupclosed.style.display = 'block';
-};
+
+// Drag drop logic
+let isDragging = false;
+let offset = { x: 0, y: 0 };
+
+header.addEventListener("mousedown", (e) => {
+  isDragging = true;
+
+  offset.x = e.clientX - popupBox.offsetLeft;
+  offset.y = e.clientY - popupBox.offsetTop;
+
+  app.style.pointerEvents = "none";
+  document.body.style.userSelect = "none";
+});
+
+window.addEventListener("mousemove", (e) => {
+  if (!isDragging) return;
+
+  // Atualiza a posição
+  popupBox.style.left = `${e.clientX - offset.x}px`;
+  popupBox.style.top = `${e.clientY - offset.y}px`;
+  popupBox.style.bottom = "auto";
+  popupBox.style.right = "auto";
+});
+
+// Finaliza o arrasto
+window.addEventListener("mouseup", () => {
+  if (isDragging) {
+    isDragging = false;
+    app.style.pointerEvents = "auto";
+    document.body.style.userSelect = "auto";
+  }
+});
